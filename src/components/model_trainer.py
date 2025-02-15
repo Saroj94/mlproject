9b62ebc9d35567695762e2cdf1ecd5c6c4c55a8f
Saroj Rai <114452296+Saroj94@users.noreply.github.com>
2025-02-15 16:24:46 +0000

    my prediction app

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -52,7 +52,55 @@ class Modeltrainer:
                 "Decision Tree regressor": DecisionTreeRegressor()
             }
 
-            model_report: dict = evaluate_model(X_train=x_train, y_train=y_train, X_test=x_test, y_test=y_test, model=models)
+            ##hyperparameter tuning
+            params={
+                "Decision Tree regressor": {'criterion':['squared_error','friedman_mse','mean_absolute','poisson'],
+                ##'splitter':['best','random'],
+                ##'max_depth':[2,3,4,5,6,7,8,9,10]
+            },
+            "Random Forest Regressor":{'n_estimators':[8,16,32,64,100,200],
+                                       ##'criterion':['mse','mae'],
+                                       # maxfeatures:['auto','sqrt','log2'],
+                                        ##'max_depth':[2,3,4,5,6,7,8,9,10]},
+            },
+            "Gradient Boost regressor":{'n_estimators':[8,16,32,64,128,256],
+                                        ##'loss':['ls','lad','huber','quantile'],
+                                        'learning_rate':[0.001,0.01,0.1,0.05],
+                                        'subsample':[0.5,0.6,0.7,0.8,0.9,1.0],
+                                        ##'criterion':['friedman_mse','mse','mae'],
+                                        ##'max_depth':[2,3,4,5,6,7,8,9,10],
+                                        ##'max_features':['auto','sqrt','log2'],
+                                        ##'warm_start':[True,False],
+                                        ##'validation_fraction':[0.1,0.2,0.3,0.4,0.5],
+                                        ##'n_iter_no_change':[5,10,15,20],
+                                        ##'tol':[0.001,0.01,0.1,0.2,0.3],
+                                        ##'ccp_alpha':[0.0,0.1,0.2,0.3,0.4,0.5]},
+            
+            },
+            "Linear Regression":{}, 
+            "Lasso":{},
+            "Ridge":{},
+            "KNeighborsRegressor":{'n_neighbors':[5,7,9,11],
+                                    ##'weights':['uniform','distance'],
+                                    ##'algorithm':['auto','ball_tree','kd_tree','brute'],
+                                    ##'leaf_size':[30,40,50,60],
+                                    ##'p':[1,2]},
+            },
+            "SVR":{'kernel':['linear','poly','rbf','sigmoid'],
+                    ##'degree':[2,3,4,5,6],
+                    ##'gamma':['scale','auto'],
+                    ##'C':[0.1,1,10,100,1000],
+                    ##'epsilon':[0.1,0.2,0.3,0.4,0.5]},
+            },
+            "AdaBoost":{'n_estimators':[8,16,32,64,128,256],
+                        'learning_rate':[0.1,0.01,0.001,0.05],
+                        ##'loss':['linear','square','exponential']},
+            },'XGBRegressor':{'n_estimators':[8,16,32,64,128,256],
+                                'learning_rate':[0.1,0.01,0.001,0.05],
+            }}
+
+            model_report: dict = evaluate_model(X_train=x_train, y_train=y_train, X_test=x_test, y_test=y_test, 
+                                                model=models,param=params)
 
             ##to get the best model score
             best_model_score = max(sorted(model_report.values()))
@@ -82,3 +130,106 @@ class Modeltrainer:
             return r2score
         except Exception as e:
             raise CustomException(e, sys)
+
+"""##Basic library
+import os
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from dataclasses import dataclass
+##modeling dependencies
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor
+from sklearn.svm import SVR
+from sklearn.linear_model import Ridge, Lasso, LinearRegression
+from sklearn.model_selection import RandomizedSearchCV
+from xgboost import XGBRegressor
+
+from src.logger import logging
+from src.exception import CustomException
+from src.utils import save_object, evaluate_model
+
+
+@dataclass
+class ModelTrainerConfig:
+    ##create variable 
+    trained_model_file_path = os.path.join('artifacts', 'model.pkl')
+
+
+class Modeltrainer:
+    ##define the function
+    def __init__(self):
+        ##initializing the path name in the variable model_trainer_config
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("Splitting train and test dataset")
+            x_train, y_train = train_array[:, :-1], train_array[:, -1]
+            x_test, y_test = test_array[:, :-1], test_array[:, -1]
+
+            models = {
+                "Linear Regression": LinearRegression(),
+                "Lasso": Lasso(),
+                "Ridge": Ridge(),
+                "SVR": SVR(),
+                "Random Forest Regressor": RandomForestRegressor(),
+                "AdaBoost": AdaBoostRegressor(),
+                "Gradient Boost regressor": GradientBoostingRegressor(),
+                "xgboost": XGBRegressor(),
+                "KNearest Neigbhour": KNeighborsRegressor(),
+                "Decision Tree regressor": DecisionTreeRegressor()
+            }
+
+            ##hyperparameter tuning
+            params = {
+                "Decision Tree regressor": {'criterion': ['squared_error', 'friedman_mse', 'mean_absolute', 'poisson']},
+                "Random Forest Regressor": {'n_estimators': [8, 16, 32, 64, 100, 200]},
+                "Gradient Boost regressor": {'n_estimators': [8, 16, 32, 64, 128, 256],
+                                             'learning_rate': [0.001, 0.01, 0.1, 0.05],
+                                             'subsample': [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]},
+                "Linear Regression": {},
+                "Lasso": {},
+                "KNeighborsRegressor": {'n_neighbors': [5, 7, 9, 11]},
+                "SVR": {'kernel': ['linear', 'poly', 'rbf', 'sigmoid']},
+                "AdaBoost": {'n_estimators': [8, 16, 32, 64, 128, 256],
+                             'learning_rate': [0.1, 0.01, 0.001, 0.05]},
+                "XGBRegressor": {'n_estimators': [8, 16, 32, 64, 128, 256],
+                                 'learning_rate': [0.1, 0.01, 0.001, 0.05]}
+            }
+
+            model_report: dict = evaluate_model(X_train=x_train, y_train=y_train, X_test=x_test, y_test=y_test, 
+                                                models=models, param=params)
+
+            ##to get the best model score
+            best_model_score = max(sorted(model_report.values()))
+            logging.info(f"Best model score is {best_model_score}")
+
+            ##get the best model name from dictionary
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)]
+
+            best_model = models[best_model_name]
+            logging.info(f"Best model is {best_model_name}")
+            if best_model_score < 0.6:
+                logging.error("Model score is less than 0.6")
+                raise CustomException("Best model score is less than 0.6", sys)
+
+            ##preprocessing we can add if we want
+
+            ##save the best model
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predict = best_model.predict(x_test)
+            r2score = r2_score(y_test, predict)
+
+            return r2score
+        except Exception as e:
+            raise CustomException(e, sys)"""
---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,85 @@
+import sys
+import os
+import pandas as pd
+import numpy as np
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import load_object
+
+class PredictionPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self, features):
+        try:
+            logging.info("Loading model and preprocessor.")
+            model_path = 'artifacts/model.pkl'
+            preprocessor_path = 'artifacts/preprocessor.pkl'
+            
+            ##read the model and preprocessor from the file_path
+            logging.info("Reading model and preprocessor from the file path.")
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+            
+            # Debugging: Check if preprocessor is None
+            if preprocessor is None:
+                raise ValueError("Preprocessor not loaded correctly!")
+
+            logging.info("Transforming features.")
+            
+            # Debugging: Print features before transformation
+            print("Features before transformation:", features)
+            
+            """# Ensure features is not None
+            if features is None:
+                raise ValueError("Features are None. Check data preprocessing!")
+
+            logging.info("Checking for missing values before transformation.")
+            features.fillna(0, inplace=True)  # Replace None/NaN with "0"""
+            logging.info("Transforming features.")
+            data_scaled = preprocessor.transform(features)
+            
+            logging.info("Making predictions.")
+            prediction = model.predict(data_scaled)
+            
+            logging.info("Prediction completed.")
+            return prediction
+        except Exception as e:
+            logging.info(f"Error in prediction: {e}")
+            raise CustomException(e, sys)
+
+##class that saves the input data and pass it to the next step wherever it is require and
+##models will use this data to predict the output
+class CustomData:
+    def __init__(self,gender: str,race_ethnicity: str,parental_level_of_education: str,lunch: str,test_preparation_course: str,writing_score: float,reading_score: float):
+                ##initialize/assigning all values into variables that are required for prediction
+                self.gender = gender
+                self.race_ethnicity = race_ethnicity  # Change to match expected column name
+                self.parental_level_of_education = parental_level_of_education  # Change to match expected column name
+                self.lunch = lunch
+                self.test_preparation_course = test_preparation_course  # Change to match expected column name
+                self.writing_score = writing_score  # Change to match expected column name
+                self.reading_score = reading_score  # Change to match expected column name
+
+    logging.info("Custom function for Converting data to DataFrame.")       
+    def get_data_as_data_frame(self):
+        ##return the data as dataframe
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race/ethnicity": [self.race_ethnicity],  # Change to match expected column name
+                "parental level of education": [self.parental_level_of_education],  # Change to match expected column name
+                "lunch": [self.lunch],
+                "test preparation course": [self.test_preparation_course],  # Change to match expected column name
+                "writing score": [self.writing_score],  # Change to match expected column name
+                "reading score": [self.reading_score]  # Change to match expected column name
+            }
+            logging.info("Converting custom data to DataFrame.")
+            return pd.DataFrame(custom_data_input_dict)
+        except Exception as e:
+            logging.error(f"Error in converting data to DataFrame: {e}")
+            raise CustomException(e, sys)
+
+
+
+
